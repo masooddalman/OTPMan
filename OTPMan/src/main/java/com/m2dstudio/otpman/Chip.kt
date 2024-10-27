7a3534cd9ed5b8876d95a028958ccc5000c5784e
masooddalman <m.dalman68@gmail.com>
2024-10-27 17:11:49 +0300

    Create Chip

---
+++ OTPMan/src/main/java/com/m2dstudio/otpman/Chip.kt
@@ -0,0 +1,106 @@
+package com.m2dstudio.otpman
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.EaseInOutCubic
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.m2dstudio.otpman.dataModel.DataModelChip
+
+enum class ChipState{
+    Normal,
+    Selected
+}
+@Composable
+fun Chip(modifier: Modifier,
+         str:String="",
+         state: ChipState,
+         normal:DataModelChip,
+         selected:DataModelChip
+         )
+{
+    val animationDuration = 250
+
+    val animatedBackColor = animateColorAsState(
+        when(state){
+            ChipState.Normal -> normal.backColor
+            ChipState.Selected -> selected.backColor
+        }, label = "backColor",
+        animationSpec = tween(durationMillis = animationDuration, easing = EaseInOutCubic)
+    )
+
+    val animatedBorderColor = animateColorAsState(
+        when(state){
+            ChipState.Normal -> normal.borderColor
+            ChipState.Selected -> selected.borderColor
+        }, label = "borderColor",
+        animationSpec = tween(durationMillis = animationDuration, easing = EaseInOutCubic)
+    )
+
+    val animatedBorderWidth = animateIntAsState(
+        when(state) {
+            ChipState.Normal -> normal.borderWidth
+            ChipState.Selected -> selected.borderWidth
+        }, label = "borderWidth",
+        animationSpec = tween(durationMillis = animationDuration, easing = EaseInOutCubic)
+    )
+
+    val animatedCornerRadius = animateIntAsState(
+        when(state) {
+            ChipState.Normal -> normal.cornerRadius
+            ChipState.Selected -> selected.cornerRadius
+        }, label = "cornerRadius",
+        animationSpec = tween(durationMillis = animationDuration, easing = EaseInOutCubic)
+    )
+
+    val animatedSize = animateIntAsState(
+        when(state) {
+            ChipState.Normal -> normal.size
+            ChipState.Selected -> selected.size
+        }, label = "size",
+        animationSpec = tween(durationMillis = animationDuration, easing = EaseInOutCubic)
+    )
+
+    Box(modifier = modifier
+        .size(animatedSize.value.dp)
+        .clip(RoundedCornerShape(animatedCornerRadius.value.dp))
+        .background(animatedBackColor.value)
+        .border(
+            width = animatedBorderWidth.value.dp,
+            color = animatedBorderColor.value,
+            shape = RoundedCornerShape(animatedCornerRadius.value.dp)
+        ),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = str, style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp))
+    }
+}
+
+
+@Preview
+@Composable
+fun SampleShapePreview()
+{
+    Chip(modifier = Modifier,
+        str = "5",
+        state = ChipState.Normal,
+        normal = DataModelChip.normal(),
+        selected = DataModelChip.selected()
+    )
+}
+
+
