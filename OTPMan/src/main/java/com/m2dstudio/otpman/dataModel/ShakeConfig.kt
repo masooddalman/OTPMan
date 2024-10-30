f0df9ec5e9df663a46e15d14d16956d8aeb91c95
masooddalman <m.dalman68@gmail.com>
2024-10-30 12:14:39 +0300

    Adding pre-made ShakeConfig

--- OTPMan/src/main/java/com/m2dstudio/otpman/dataModel/ShakeConfig.kt
+++ OTPMan/src/main/java/com/m2dstudio/otpman/dataModel/ShakeConfig.kt
@@ -11,4 +11,23 @@ data class ShakeConfig(
     val translateX: Float = 0f,
     val translateY: Float = 0f,
     val trigger: Long = System.currentTimeMillis(),
-)
+) {
+    companion object {
+        val error = ShakeConfig(
+            iterations = 4,
+            intensity = 2_000f,
+            rotateY = 30f,
+            translateX = 20f,
+        )
+
+        val success = ShakeConfig(
+            iterations = 4,
+            intensity = 2_000f,
+            rotateX = 30f,
+            translateY = 20f,
+        )
+
+        val none = ShakeConfig(iterations = 0)
+    }
+
+}
