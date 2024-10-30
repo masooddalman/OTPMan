2ed729136d17fc65d3993ae6c4d442859bb452bc
masooddalman <m.dalman68@gmail.com>
2024-10-30 12:13:01 +0300

    Create ShakeController

---
+++ OTPMan/src/main/java/com/m2dstudio/otpman/controller/ShakeController.kt
@@ -0,0 +1,22 @@
+package com.m2dstudio.otpman.controller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.m2dstudio.otpman.dataModel.ShakeConfig
+
+@Composable
+fun rememberShakeController(): ShakeController {
+    return remember { ShakeController() }
+}
+
+class ShakeController {
+    var shakeConfig: ShakeConfig? by mutableStateOf(null)
+        private set
+
+    fun shake(shakeConfig: ShakeConfig) {
+        this.shakeConfig = shakeConfig
+    }
+}
