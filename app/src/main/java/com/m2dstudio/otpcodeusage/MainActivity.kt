022770f6ac12316da1d17bdb01e6ee6582f9eb06
masooddalman <m.dalman68@gmail.com>
2024-11-08 13:27:21 +0300

    Implement OTPMan in sample app MainActivity

--- app/src/main/java/com/m2dstudio/otpcodeusage/MainActivity.kt
+++ app/src/main/java/com/m2dstudio/otpcodeusage/MainActivity.kt
@@ -4,20 +4,89 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.m2dstudio.otpcodeusage.ui.theme.OTPCodeUsageTheme
+import com.m2dstudio.otpman.AnimationType
+import com.m2dstudio.otpman.OTPMan
+import com.m2dstudio.otpman.OTPState
 
 class MainActivity : ComponentActivity() {
+
+    private var otpState = mutableStateOf(OTPState.Idle)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
         setContent {
             OTPCodeUsageTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .padding(horizontal = 24.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,) {
+                        OTPMan(modifier = Modifier, count = 5,
+                            animationType = AnimationType.Shake,
+                            otpState = otpState.value,
+                            onValueChange = {
+                                otpState.value = OTPState.Idle
+                            },
+                            onComplete = {
+                            println("input code is $it")
+                        },
+                            onAnimationDone = {
+                                println("animation done $it")
+                            })
+                        Spacer(modifier = Modifier.padding(16.dp))
+                        Button(onClick = {
+                            otpState.value = OTPState.Success
+                        }) {
+                            Text(text = "send otp code to server and success")
+                        }
+
+                        Button(onClick = {
+                            otpState.value = OTPState.Failed
+                        }) {
+                            Text(text = "send otp code to server and failed")
+                        }
+
+                    }
 
                 }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainActivityPreview() {
+    OTPCodeUsageTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPassing ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPassing)
+                    .padding(horizontal = 24.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,) {
+                OTPMan(modifier = Modifier, count = 5, onComplete = {})
             }
         }
     }
