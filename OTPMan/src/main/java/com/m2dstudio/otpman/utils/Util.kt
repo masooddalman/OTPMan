555b766f746b5477ae7f4bde955df638b07a5940
masooddalman <m.dalman68@gmail.com>
2024-11-10 15:55:44 +0300

    adding generateAppHashKey function to Util

--- OTPMan/src/main/java/com/m2dstudio/otpman/utils/Util.kt
+++ OTPMan/src/main/java/com/m2dstudio/otpman/utils/Util.kt
@@ -1,9 +1,17 @@
 package com.m2dstudio.otpman.utils
 
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.util.Base64
 import android.util.Log
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.geometry.center
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.Arrays
 import kotlin.math.PI
 import kotlin.math.abs
 import kotlin.math.acos
@@ -51,4 +59,69 @@ fun getGradientCoordinates(chipSize:Int, normalizedAngle: Float, angleInRadians:
     val end = clampOffset(size.center + Offset(horizontalOffset, -verticalOffset), size)
 
     return start to end
+}
+
+private const val HASH_TYPE = "SHA-256"
+const val NUM_HASHED_BYTES: Int = 9
+const val NUM_BASE64_CHAR: Int = 11
+fun generateAppHashKey(context: Context) {
+    val appCodes = ArrayList<String>()
+    try {
+
+        // Get all package signatures for the current package
+        val packageName = context.packageName
+        val packageManager = context.packageManager
+        val signatures = packageManager.getPackageInfo(
+            packageName,
+            PackageManager.GET_SIGNATURES
+        ).signatures
+
+        // For each signature create a compatible hash
+        for (signature in signatures) {
+            val hash = hash(packageName, signature.toCharsString())
+            if (hash != null) {
+                appCodes.add(String.format("%s", hash))
+            }
+        }
+    } catch (e: PackageManager.NameNotFoundException) {
+        print("Unable to find package to obtain hash.\n$e", true)
+    }
+    val isDebuggable = 0 != context.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+    print("================== App hash key ==================", isDebuggable)
+    print("=", isDebuggable)
+    print("=", isDebuggable)
+    print("= package name = ${context.packageName}", isDebuggable)
+    print("=", isDebuggable)
+    print("=      👉👉👉👉${appCodes.first()}👈👈👈👈   ", isDebuggable)
+    print("=", isDebuggable)
+    print("=", isDebuggable)
+    if(isDebuggable)
+    {
+        print("= be careful, this hash key only works for debug version, don't use it in production", true)
+        print("= please run the project in release mode to generate release hash key", true)
+    }
+    print("= usage : https://github.com/masooddalman/OTPMan", isDebuggable)
+    print("================== App hash key ==================", isDebuggable)
+}
+
+private fun hash(packageName: String, signature: String): String? {
+    val appInfo = "$packageName $signature"
+    try {
+        val messageDigest = MessageDigest.getInstance(HASH_TYPE)
+        messageDigest.update(appInfo.toByteArray(StandardCharsets.UTF_8))
+        var hashSignature = messageDigest.digest()
+
+        // truncated into NUM_HASHED_BYTES
+        hashSignature = Arrays.copyOfRange(hashSignature, 0, NUM_HASHED_BYTES)
+        // encode into Base64
+        var base64Hash =
+            Base64.encodeToString(hashSignature, Base64.NO_PADDING or Base64.NO_WRAP)
+        base64Hash = base64Hash.substring(0, NUM_BASE64_CHAR)
+
+        return base64Hash
+    } catch (e: NoSuchAlgorithmException) {
+        print("hash:NoSuchAlgorithm\n$e",true)
+
+    }
+    return null
 }
