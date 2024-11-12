10259de62ff8fd148e415cfd50bd73375369b97a
masooddalman <m.dalman68@gmail.com>
2024-11-12 22:31:07 +0300

    Adding otpManViewModel to MainActivity

--- app/src/main/java/com/m2dstudio/otpcodeusage/MainActivity.kt
+++ app/src/main/java/com/m2dstudio/otpcodeusage/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,17 +14,27 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.m2dstudio.otpcodeusage.ui.theme.OTPCodeUsageTheme
-import com.m2dstudio.otpman.AnimationType
 import com.m2dstudio.otpman.OTPMan
 import com.m2dstudio.otpman.OTPState
+import com.m2dstudio.otpman.viewModel.OTPManViewModel
 
 class MainActivity : ComponentActivity() {
+    private val otpManViewModel by viewModels<OTPManViewModel>{
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return OTPManViewModel(
+                    count = 5
+                ) as T
+            }
+        }
+    }
 
     private var otpState = mutableStateOf(OTPState.Idle)
     override fun onCreate(savedInstanceState: Bundle?) {
