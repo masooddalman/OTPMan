099f65b3ee730dbb453e8b1eec262236b8b66055
masooddalman <m.dalman68@gmail.com>
2024-11-18 10:13:59 +0300

    implement OTPManCountDown

--- app/src/main/java/com/m2dstudio/otpcodeusage/MainActivity.kt
+++ app/src/main/java/com/m2dstudio/otpcodeusage/MainActivity.kt
@@ -11,24 +11,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.m2dstudio.otpcodeusage.ui.theme.OTPCodeUsageTheme
+import com.m2dstudio.otpman.CountDownMode
 import com.m2dstudio.otpman.OTPMan
 import com.m2dstudio.otpman.OTPManCountDown
 import com.m2dstudio.otpman.utils.generateAppHashKey
@@ -72,6 +66,8 @@ class MainActivity : ComponentActivity() {
                             onAnimationDone = {
                                 Log.v("MainActivity", "animation done $it")
                             })
+                        Spacer(modifier = Modifier.padding(16.dp))
+                        OTPManCountDown( secondsInFuture = 70 , mode = CountDownMode.MinutesThenSeconds)
                         Spacer(modifier = Modifier.padding(16.dp))
                         Button(onClick = {
                             otpManViewModel.makeItSuccess()
